b09bd232f0b829a64d397e2fdc1545dbaa02a239
nexpassword99 <inderscop@gmail.com>
2025-03-26 17:03:30 -0500

    refactor: Usar modelo seleccionado del la configuración para evitar modificar para cada modelo

--- app/interface/api/routes/chat.py
+++ app/interface/api/routes/chat.py
@@ -3,9 +3,10 @@ from app.interface.api.schemas import ChatResponseSchema, ChatRequestSchema
 from app.domain.services.order_service import OrderService
 from app.interface.api.dependencies import get_order_service
 from rasa.core.agent import Agent
+from app.core.config import Settings
 
 router = APIRouter()
-agent = Agent.load("models/nlu-20250326-125852-elegant-adversary.tar.gz")
+agent = Agent.load(f"models/{Settings.MODEL_SELECTED}")
 
 
 @router.post("/chat", response_model=ChatResponseSchema)
