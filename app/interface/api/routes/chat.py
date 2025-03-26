ec9bedd4437717748d869bb16b2d2d2128585489
nexpassword99 <inderscop@gmail.com>
2025-03-26 15:07:29 -0500

    feat: Actualizar endpoint de chat par manejar los mensajes y retornar la respuesta del chatbot

--- app/interface/api/routes/chat.py
+++ app/interface/api/routes/chat.py
@@ -1,17 +1,18 @@
 from fastapi import APIRouter, Depends
-from app.interface.api.schemas import OrderResponseSchema
+from app.interface.api.schemas import ChatResponseSchema, ChatRequestSchema
 from app.domain.services.order_service import OrderService
+from app.interface.api.dependencies import get_order_service
+from rasa.core.agent import Agent
 
 router = APIRouter()
+agent = Agent.load("models/nlu-20250326-125852-elegant-adversary.tar.gz")
 
 
-@router.post("/chat", response_model=OrderResponseSchema)
+@router.post("/chat", response_model=ChatResponseSchema)
 async def handle_chat_message(
-    message: str,
-    order_service: OrderService = Depends(get_order_service)
+    chat_message: ChatRequestSchema,
 ):
-    order = order_service.create_order(
-        items=message.split(','),
-        customer_id="user123")
+    response = await agent.handle_text(chat_message.message)
+    chatbot_response = response[0]["text"] if response else "Lo siento, no entendí eso."
 
-    return OrderResponseSchema(id=order.id, status=order.status, items=order.items)
+    return ChatResponseSchema(response=chatbot_response)
