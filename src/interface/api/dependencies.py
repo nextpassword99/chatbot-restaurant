536505cdfa2e975df0a64826b38568ca11aa6538
nexpassword99 <inderscop@gmail.com>
2025-03-26 09:46:08 -0500

    feat: Añadir dependencias para la gestión de la base de datos y servicio de pedidos

---
+++ src/interface/api/dependencies.py
@@ -0,0 +1,17 @@
+from fastapi import Depends
+from src.infrastructure.database.session import DatabaseSession
+from src.domain.repositories.order_repository import SQLOrderRepository
+from src.domain.services.order_service import OrderService
+
+
+def get_db():
+    db = DatabaseSession("sqlite:///orders.db")
+    try:
+        yield db.get_session()
+    finally:
+        db.get_session().close()
+
+
+def get_order_service(db=Depends(get_db)) -> OrderService:
+    repo = SQLOrderRepository(db)
+    return OrderService(repo)
