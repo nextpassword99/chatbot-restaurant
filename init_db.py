36c8d41fd2b5876fc7ee4e2863ead317e7b815a2
nexpassword99 <inderscop@gmail.com>
2025-03-26 09:43:17 -0500

    feat: Añadir script de inicialización de la base de datos

---
+++ init_db.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from src.domain.models.order_model import Base
+from src.core.config import settings
+
+
+engine = create_engine(settings.DB_URL)
+
+
+Base.metadata.create_all(bind=engine)
+
+print("Base de datos inicializada")
