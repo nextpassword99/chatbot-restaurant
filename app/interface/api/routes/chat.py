4c30bd6d821e8681e31fa5ab7685378e739a39ff
nexpassword99 <inderscop@gmail.com>
2025-03-27 23:02:56 -0500

    refactor: Manejar instancia de agencia

--- app/interface/api/routes/chat.py
+++ app/interface/api/routes/chat.py
@@ -1,23 +1,21 @@
 from fastapi import APIRouter, Depends
 from app.interface.api.schemas import ChatResponseSchema, ChatRequestSchema
-from rasa.core.agent import Agent
-from app.core.config import settings
 from app.interface.api.execute import Execute
 
 router = APIRouter()
+agent_instance = None
 
 
 @router.post("/chat", response_model=ChatResponseSchema)
 async def handle_chat_message(
     chat_message: ChatRequestSchema,
 ):
-    if chat_message.model == "rasa":
-        agent = Agent.load(settings.MODEL_SELECTED)
-        response = await agent.handle_text(chat_message.message)
-        chatbot_response = response[0]["text"] if response else "Lo siento, no entendí eso."
+    global agent_instance
 
-    if chat_message.model == "chatterbot":
-        agent = await Execute.start_chatter_bot()
-        chatbot_response = await Execute.handle_chat_chatter(agent, chat_message.message)
+    if agent_instance is None:
+        agent_instance = Execute.get_model(chat_message.model)
 
-    return ChatResponseSchema(response=chatbot_response)
+    chatbot_response = await agent_instance.handle_text(chat_message.message)
+    res = agent_instance.justResponse(chatbot_response)
+
+    return ChatResponseSchema(response=res)
