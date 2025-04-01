76c69bb29a85ef37d79c596b26bad9a13dc10d28
nexpassword99 <inderscop@gmail.com>
2025-04-01 16:23:59 -0500

    feat: Agregar opción para entrenar ChatterBot en el script de entrenamiento

--- trainer.py
+++ trainer.py
@@ -1,10 +1,23 @@
 from app.application.training.model_trainer import ModelTrainer
+from app.application.training.chatter_trainer import chatbotTrainer
 
 
-config_path = "config.yml"
-domain_path = "domain.yml"
-training_data_dir = "data"
+opt = input('''
+            Entrenar: 
+            1 - Rasa
+            2 - ChatterBot
+            ''')
 
+if opt == '1':
+    config_path = "config.yml"
+    domain_path = "domain.yml"
+    training_data_dir = "data"
 
-trainer = ModelTrainer(config_path, domain_path, training_data_dir)
-trainer.train(output_dir="models")
+    trainer = ModelTrainer(config_path, domain_path, training_data_dir)
+    trainer.train(output_dir="models")
+elif opt == '2':
+    trainer = chatbotTrainer()
+    trainer.train()
+
+else:
+    print("Opcion no valida")
