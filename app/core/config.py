c8f02be9b41939c54ce260940c6c214b7bac8304
nextpassword99 <inderscop@gmail.com>
2025-04-01 22:17:38 -0500

    feat: Agregar variable de entorno MODEL_GEMMA para configuración del modelo Gemma

--- app/core/config.py
+++ app/core/config.py
@@ -10,6 +10,7 @@ class Settings(BaseSettings):
     MODEL_PATH: str = os.getenv("MODEL_PATH", "models")
     TRAINING_DATA_DIR: str = os.getenv("TRAINING_DATA_DIR", "data")
     MODEL_SELECTED = str = os.getenv("MODEL_SELECTED", "model")
+    MODEL_GEMMA = str = os.getenv("MODEL_GEMMA", "gemma3:1b")
 
     class Config:
         env_file = ".env"
