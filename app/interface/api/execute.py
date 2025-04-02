9d573d00bf007965ab73f5029d2c275b72836cfd
nextpassword99 <inderscop@gmail.com>
2025-04-01 22:17:27 -0500

    feat: Añadir soporte para el modelo Gemma en el método get_model

--- app/interface/api/execute.py
+++ app/interface/api/execute.py
@@ -2,16 +2,20 @@ from chatterbot import ChatBot
 from app.core.config import settings
 from app.application.models.rasa import RasaModel
 from app.application.models.chatterbot import ChatterBot
+from app.application.models.gemma import GemmaModel
 
 
 class Execute:
     @staticmethod
-    def get_model(model) -> RasaModel | ChatterBot | None:
+    def get_model(model) -> RasaModel | ChatterBot | GemmaModel | None:
         if model == "rasa":
             return RasaModel()
 
         if model == "chatterbot":
             return ChatterBot()
+
+        if model == "gemma":
+            return GemmaModel()
 
     @staticmethod
     async def start_chatter_bot():
