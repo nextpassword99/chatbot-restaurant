8285b0d135448b135a37a56192a38ac226ebf459
nexpassword99 <inderscop@gmail.com>
2025-04-03 11:45:40 -0500

    feat: Añadir clase Gemma2 con nueva forma ejecutar el modelo Gemma

---
+++ app/application/models/gemma2.py
@@ -0,0 +1,17 @@
+from app.core.config import settings
+import subprocess
+
+
+class Gemma2:
+    async def load_model(self):
+        return self
+
+    async def handle_text(self, msg):
+        result = subprocess.run(f"ollama run {settings.MODEL_GEMMA} {msg}",
+                                shell=True,
+                                capture_output=True)
+
+        return result.stdout.decode("utf-8")
+
+    async def justResponse(self, response):
+        return response
