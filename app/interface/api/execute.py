05759e4f4a40be3df7aaf4acaca7b8a872fcddec
nexpassword99 <inderscop@gmail.com>
2025-04-03 12:00:05 -0500

    feat: Añadir Agente Gemma2

--- app/interface/api/execute.py
+++ app/interface/api/execute.py
@@ -4,11 +4,12 @@ from app.application.models.rasa import RasaModel
 from app.application.models.chatterbot import ChatterBot
 from app.application.models.gemma import GemmaModel
 from app.application.models.deepseek import DeepSeekModel
+from app.application.models.gemma2 import Gemma2
 
 
 class Execute:
     @staticmethod
-    def get_model(model) -> RasaModel | ChatterBot | GemmaModel | DeepSeekModel | None:
+    def get_model(model) -> RasaModel | ChatterBot | GemmaModel | DeepSeekModel | Gemma2 | None:
         if model == "rasa":
             return RasaModel()
 
@@ -20,3 +21,6 @@ class Execute:
 
         if model == "deepseek":
             return DeepSeekModel()
+
+        if model == "gemma2":
+            return Gemma2()
